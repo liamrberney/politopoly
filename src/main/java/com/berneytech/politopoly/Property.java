d02c6615915a26ab34271c89a761af50ae59df61
bernelia000 <bernelia000@MHSR104-890JHK2.sbm1.springbranchisd.com>
2019-02-07 08:28:34 -0600

    Oh wait no I'm dumb

--- src/main/java/com/berneytech/politopoly/Property.java
+++ src/main/java/com/berneytech/politopoly/Property.java
@@ -64,12 +64,15 @@ public class Property implements Space{
         out.println(player+" pays "+owner+" $"+rent[rentStatus]+" for rent");
     }
     void buyDialogue(Player player){
+        List<Player> players= new ArrayList(Mechanics.getPlayers());
+        
         out.println("Would "+player+" like to buy "+name+" for "+price+"? [yes/no]");
         boolean decision=decision();
         if (decision){
             if(player.getBalance()>price){
                 owned=1;
                 owner=player;
+                player.setBalance(-price);
                 player.addSpace(this);
                 Mechanics.setPlayer(player);
             }
@@ -77,11 +80,29 @@ public class Property implements Space{
         else{ 
             if (Mechanics.getNumPlayers()==2){
                 out.println("Would the other player like to buy "+name+" for "+price+"? [yes/no]");
+                int indexOne=Mechanics.getPlayerIndex(player);
                 decision=decision();
-                if(player.getBalance()>price&&decision){
-                    owned=1;
-                    player.addSpace(this);
-                    Mechanics.setPlayer(player);
+                if (indexOne==0){
+                    if(players.get(1).getBalance()>price&&decision){
+                        owned=1;
+                        players.get(1).setBalance(-price);
+                        players.get(1).addSpace(this);
+                        Mechanics.setPlayer(players.get(1));
+                    }
+                    else{
+                        out.println("sorry, you're poor");
+                    }
+                }
+                if (indexOne==1){
+                    if(players.get(0).getBalance()>price&&decision){
+                        owned=1;
+                        players.get(0).setBalance(-price);
+                        players.get(0).addSpace(this);
+                        Mechanics.setPlayer(players.get(0));
+                    }
+                    else{
+                        out.println("sorry, you're poor");
+                    }
                 }
             }
             else{
@@ -109,18 +130,21 @@ public class Property implements Space{
     void beginAuction(){
         List<Player> players= new ArrayList<Player>(Mechanics.getPlayers());
         int maxBid=0;
-        Player winner=new Player("yeet");
+        Player winner=players.get(0);
         for (int x=0; x<players.size(); x++){
             out.println("Would "+players.get(x)+" like to bid?");
             if (decision()){
                 out.print("Enter bid amount: ");
                 int bid=bid();
-                if (bid>maxBid){
+                if (bid>maxBid&&bid<=players.get(x).getBalance()){
                     maxBid=bid;
                     winner=players.get(x);
                 }
-                else
+                else{
                     out.println("IT'S FOR THE CHURCH HONEY. NEXT!!!");
+                    players.remove(x);
+                    x--;
+                }
             }
             else{
                 players.remove(x);
@@ -132,7 +156,12 @@ public class Property implements Space{
             }
         }
         out.println(winner+" won "+name+" for $"+maxBid);
-        
+        winner.setBalance(-maxBid);
+        winner.addSpace(this);
+        Mechanics.setPlayer(winner);
+    }
+    public String toString(){
+        return name;
     }
 
     @Override
