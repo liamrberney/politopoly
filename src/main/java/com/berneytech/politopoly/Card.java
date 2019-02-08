10b434a64bbcd8ff82eb4ae74044b3613c64de36
bernelia000 <bernelia000@MHSR104-890JHK2.sbm1.springbranchisd.com>
2019-02-08 08:35:29 -0600

    added stuff

--- src/main/java/com/berneytech/politopoly/Card.java
+++ src/main/java/com/berneytech/politopoly/Card.java
@@ -19,7 +19,22 @@ public class Card {
         Card a= Board.getCommunityChest((int)Math.random()*17);
         out.println(a.getName());
         out.println(a.getText());
-        if (a.getSendTo()==-1){}
+        if (a.getSendTo()==-1){
+            switch (a.getType()){
+                case "PAYMENT":
+                player.setBalance(a.getRent());
+                break;
+                case "REDISTRIBUTION":
+                    List b= new ArrayList(Mechanics.getPlayers());
+                case "GETOUTOFJAILFREE":
+                    player.setGetOutOfJailFree(1);
+                    break;
+                case "STREETREPAIRS":
+                case "REPAIRS":
+                default:
+                    out.println("HMMMM");
+        }
+        }
         else{
             if (!a.getType().equals("GOTOJAIL")){
                 if (a.getSendTo()<player.getLocation()){
@@ -36,13 +51,14 @@ public class Card {
         
 
     static void drawChance(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
+        
     }
-    public String name;
-    public String type;
-    public String text;
-    public int sendTo;
-    public int rent;
+    public  String name;
+    public  String type;
+    public  String text;
+    public  int sendTo;
+    public  int rent;
     public Card(String name, String type, String text, int sendTo, int rent){
     this.name=name;
     this.type=type;
